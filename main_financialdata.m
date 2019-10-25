2ab5da89d0625eafe5563bba4b516ee802eabca0
sayakrc <sayak261090@gmail.com>
2019-10-25 19:36:39 +0530

    Add files via upload

--- main_financialdata.m
+++ main_financialdata.m
@@ -1,3 +1,6 @@
+% Script for stock market data experiments 
+% produces cumulative regret after T rounds
+
 T = 10^3;
 
 fin = importfile('fin.csv', 2, 824);
@@ -11,16 +14,16 @@ f_test = bsxfun(@rdivide,mean_stocks,max(mean_stocks));
 K = cov(noise);
 B = max(mean_stocks);
 
-noise_index = 3; 
-noise_param = data; 
+noise_index = 3; % denotes real life heavy-tailed data
+noise_param = data; % heavy-tailed payoffs
 alpha = 1; 
 v = mean(data(:).^2);
 
 lambda = 1;
 tgp_ucb_regret = tgp_ucb(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v);
 
-eps = 0.1;
-ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,eps);
+epsilon = 0.1;
+ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,epsilon);
 
 
 
--- main_lightsensordata.m
+++ main_lightsensordata.m
@@ -1,3 +1,6 @@
+% Script for light sensor data experiments 
+% produces cumulative regret after T rounds
+
 T = 10^3;
 
 load('iw_data_all.mat');
@@ -11,16 +14,16 @@ K = cov(noise);
 f_test = bsxfun(@rdivide,mean(rawtestdata),max(mean(rawtestdata)));
 B = max(mean(rawtestdata));
 
-noise_index = 3;
-noise_param = rawtestdata; 
+noise_index = 3; % denotes real life heavy-tailed data
+noise_param = rawtestdata; % heavy-tailed payoffs
 alpha = 1; 
 v = mean(rawtestdata(:).^2);
 
 lambda = 1;
 tgp_ucb_regret = tgp_ucb(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v);
 
-eps = 0.1;
-ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,eps);
+epsilon = 0.1;
+ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,epsilon);
 
 points = data.coords;
 n = size(points,1);
@@ -28,8 +31,8 @@ r = range(points);
 m = min(points);
 sensor_location = (points - repmat(m,n,1))./repmat(r,n,1);
 l = sqrt(0.1);
-qff_features = 16;
-Phi_qff = qff_approx(sensor_location',l,qff_features);
+m_bar = 16;
+Phi_qff = qff_approx(sensor_location',l,m_bar);
 ata_gp_ucb_qff_regret = ata_gp_ucb_qff(B,f_test,Phi_qff,T,lambda,noise_index,noise_param,alpha,v);
 
 
