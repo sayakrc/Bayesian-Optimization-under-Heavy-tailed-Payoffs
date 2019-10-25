46d9923e284fdeebb980ddd9ae47699d45ff9b2a
sayakrc <sayak261090@gmail.com>
2019-10-25 19:36:04 +0530

    Add files via upload

--- main_synthetic.m
+++ main_synthetic.m
@@ -1,8 +1,11 @@
+% Script for synthetic data experiments 
+% produces cumulative regret after T rounds
+
 T = 10^3;
 
 data = 0.01:0.01:1;
 [d,arms] = size(data);
-kernel_index = 1;
+kernel_index = 1;   % use 1 for SE kernel and 2 for Matern kernel
 l = 0.2;
 K = zeros(arms);
 for i = 1 : arms
@@ -13,19 +16,19 @@ for i = 1 : arms
     end
 end
 
-rkhs_index = 1;
+rkhs_index = 1;  % use 1 for RKHS functions with coefficients in [-1,1] and 2 for coefficients in [0,1]
 p = 100;
 f = gen_rkhs_function(data,l,p,rkhs_index,kernel_index);
 f_test = bsxfun(@rdivide,f,max(abs(f)));
 B = max(abs(f_test));
 
-noise_index = 1;
+noise_index = 1; % use 1 for Students-t distribution and 2 for Pareto distribution
 if noise_index == 1
-    noise_param = 3;
+    noise_param = 3; % degrees of freedom of the Students-t distribution
     alpha = 1;
     v = noise_param/(noise_param-2)+B^2;
 else
-    noise_param = 2;
+    noise_param = 2; % shape paremeter of Pareto distribution
     alpha = 0.9;
     v = (B^(1+alpha))/((2^alpha)*(1-alpha));
 end
@@ -33,11 +36,11 @@ end
 lambda = 1;
 tgp_ucb_regret = tgp_ucb(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v);
 
-eps = 0.1;
-ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,eps);
+epsilon = 0.1;
+ata_gp_ucb_nystrom_regret = ata_gp_ucb_nystrom(B,f_test,K,T,lambda,noise_index,noise_param,alpha,v,epsilon);
 
-qff_features = 32;
-Phi_qff = qff_approx(data,l,qff_features);
+m_bar = 32;
+Phi_qff = qff_approx(data,l,m_bar);
 ata_gp_ucb_qff_regret = ata_gp_ucb_qff(B,f_test,Phi_qff,T,lambda,noise_index,noise_param,alpha,v);
 
 
